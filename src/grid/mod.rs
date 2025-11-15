80323f606651ee98fa8aced79bbdffcef67a885b
cboltjes <cbolt117@gmail.com>
2025-11-15 00:17:38 -0600

    Added Attractions, AttractionGrid, and Peeps

--- src/grid/mod.rs
+++ src/grid/mod.rs
@@ -1,11 +1,17 @@
 use bevy::prelude::*;
 use bevy::app::Plugin;
 
+pub mod grid;
+pub mod attraction;
+
+use grid::AttractionGrid;
+
 pub struct GridPlugin;
 
 impl Plugin for GridPlugin {
     fn build(&self, app: &mut App) {
         app
+            .insert_resource(AttractionGrid::new(UVec2::new(25,25)))
             //.add_systems(Update, (add_health_bars, update_health_bars))
         ;
     }
--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@ use crate::{grid::GridPlugin, ui::UiPlugin, casino::CasinoPlugin};
 
 pub mod ui;
 pub mod grid;
+pub mod peeps;
 pub mod casino;
 
 fn main() {
     App::new()
-        .add_plugins((DefaultPlugins, UiPlugin, GridPlugin, CasinoPlugin))
+        .add_plugins((DefaultPlugins.set(ImagePlugin::default_nearest()), UiPlugin, GridPlugin, CasinoPlugin))
     .run();
 }
